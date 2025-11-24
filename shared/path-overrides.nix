4895f9107d891aa5fc2bc9c54737297e9014f381
Ven <209770139+venvaneless@users.noreply.github.com>
2025-11-24 22:30:29 +0100

    - Trying to fix endless recursion in one of the 'path' files 2025-11-24; 22-30 - Removing 'pkgs' attribute

--- shared/path-overrides.nix
+++ shared/path-overrides.nix
@@ -10,7 +10,7 @@
 #   config.sharedPaths.<name>
 # ============================================================
 
-{ config, lib, pkgs, ... }:
+{ config, lib, ... }:
 
 let
   
@@ -25,8 +25,8 @@ in
     ++ lib.optional isLinux  ../hosts/linux/paths-linux.nix;
 
   # Merge all path namespaces into one public API
-  # config.sharedPaths =
-  # (config.pathsShared or {})
-  # // (config.pathsDarwin or {})
-  #  // (config.pathsLinux  or {});
+  config.sharedPaths =
+    (config.pathsShared or {})
+    // (config.pathsDarwin or {})
+    // (config.pathsLinux  or {});
 }
