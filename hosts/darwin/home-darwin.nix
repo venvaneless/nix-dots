e006367673e87117830694b8d49419d8b27d72da
Ven <209770139+venvaneless@users.noreply.github.com>
2025-11-25 12:18:16 +0100

    Fixing the multi-machine flake to work with machine-aware HM. Change wrong path ffrom correct path for 'home-shared.nix' in 'home-darwin.nix' 2025-11-25; 12:17

--- hosts/darwin/home-darwin.nix
+++ hosts/darwin/home-darwin.nix
@@ -26,7 +26,7 @@
     # --- DARWIN HOME-MANAGER MODULES HERE ----
     users.ven = {
       imports = [
-        ../../shared/home/home-shared.nix
+        ../../shared/home-shared.nix
       ];
 
       # --- DARWIN HOME-MANAGER PATH ----
